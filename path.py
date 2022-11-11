1c030d3d915ed19f53e32a655652dd4057813fe0
Mingyu Park <alsrb4298@naver.com>
2022-11-12 03:02:48 +0900

    fix: base_dir 가져오는 방식 수정

--- path.py
+++ path.py
@@ -1,7 +1,6 @@
 import os
 
-BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(
-    os.path.abspath(__file__))), 'whatisbreed-backend')
+BASE_DIR = os.path.join(os.getcwd())
 ORIGIN_IMG_DIR = os.path.join(BASE_DIR, 'static', 'originImg/')
 LIME_IMG_DIR = os.path.join(BASE_DIR, 'static', 'limeImg/')
 SERVER_IMG_DIR = os.path.join(
